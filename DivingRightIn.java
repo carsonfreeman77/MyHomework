b1286bafb744e2796141de26db5e95190dae4916
Carson.Freeman <Carson.Freeman@MU-C1-08.mullen.pvt.k12.co.us>
2017-08-25 09:07:47 -0600

    8/25/17

--- DivingRightIn.java
+++ DivingRightIn.java
@@ -1,60 +1,61 @@
 // HEY I DID THIS AT HOME USING GIT!!!! YAY
 
-package DivingRightIn;
+
+
 
 import java.util.Scanner;
 
-public class DivingRightin {
+public class DivingRightIn {
 
 public static void main(String[] args) { 
-	AskUserName();
-	AskAgeAndBirth(); 
-	UsersAgeInYears(); 
-	AgeInDogYears(); 
+	//AskUserName(); 
+	AskAgeAndBirth();
+	AgeInDogYears();
 	HowManyLeapYears();
-
-	}
-
-		
+}
 
 private static void AskUserName()
 	{
-	String importedName;
 	Scanner name = new Scanner(System.in);		
 	System.out.println("Hello! What is your name?");
-	importedName = name.nextLine();
+	String importedName = name.nextLine();
 	System.out.println("Hello, " + importedName + "!");
 	
 		
 	}
 
 private static void AskAgeAndBirth()
-	{
-	
+	{	
 	Scanner age = new Scanner(System.in);
 	System.out.println("How old are you?");
-	nextInt ageOfUser = age.nextInt();
+	int ageOfUser = age.nextInt();
 	
 	
 	if (ageOfUser < 16) 
-	{
-    System.out.println("Aw this is gonna be easy for me!");
-	} 
+		{
+	    System.out.println("Aw this is gonna be easy for me!");
+		} 
+		
+		if (ageOfUser == 17) 
+		{
+		System.out.println("Haha awesome so is my boss!");
+		
+		} 
+		if (ageOfUser > 18) 
+		{
+		System.out.println("Oh man this might be a lot...");
+		}
 	
-	if else (ageOfUser == 17) 
-	{
-	System.out.println("Haha awesome so is my boss!");
+	Scanner year = new Scanner(System.in);
+	System.out.println("What year were you born in?");
+	int inportedYear = year.nextInt();
 	
-	} 
-	else (ageOfUser > 18) 
-	{
-	System.out.println("Oh man this might be a lot...");
-	}
+	for(int x = 0; x <= ageOfUser; x++)
+		       {
+		       System.out.println("In " + (inportedYear + x) + " you were " + x);
+		       }
 	
-	
-	}
-private static void UsersAgeInYears()
-	{
+		
 	
 		
 	}
